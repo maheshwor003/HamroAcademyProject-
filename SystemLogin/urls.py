f3cc14490710620ec7f8003fc0aca7b2a561a530
gauravkc1 <gaurav166.gk@gmail.com>
2022-07-02 20:42:00 +0545

    design for admin home and login

--- SystemLogin/urls.py
+++ SystemLogin/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from  SystemLogin import views
 urlpatterns = [
  
-    path('', views.home, name='home'),
+    path('', views.homepage, name='homepage'),
     path('register/', views.register, name='register'),
     path('login/', views.login_view, name='login_view'),
 
