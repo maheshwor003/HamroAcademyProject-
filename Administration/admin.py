4933ad0778806fd5a83c3ad0e15d715f458d9ade
maheshwor003 <maheshworshakha33@gmail.com>
2022-09-01 09:06:09 +0545

    Model added for project

--- Administration/admin.py
+++ Administration/admin.py
@@ -1,13 +1,20 @@
 from django.contrib import admin
-from .models import Course, Instructor
+from .models import Course, Instructor,Room,Section,Department,Meetingtime
 
 
-# class CourseContent(admin.ModelAdmin):
-#      list_display= ('course_number','course_name','max_numb_students','instructors')
 
 # class InstructorDetails(admin.ModelAdmin):
 #      list_sdisplay= ('course_id','instructor_id')
 
-admin.site.register(Course)
+@admin.register(Course)
+class CourseContent(admin.ModelAdmin):
+      list_display= ('course_code','course_name','max_numb_students','instructors')
+
 admin.site.register(Instructor)
+admin.site.register(Room)
+admin.site.register(Meetingtime)
+admin.site.register(Department)
+admin.site.register(Section)
+
+
 # Register your models here.
--- Administration/models.py
+++ Administration/models.py
@@ -13,6 +13,51 @@ class Course(models.Model):
     max_numb_students = models.CharField(max_length=65, null=True)
     instructors = models.ForeignKey(Instructor,on_delete=models.CASCADE,null=True,blank=True)
 
+    def __str__(self):
+
+        return self.course_name
 
         
 
+class Room(models.Model):
+    Room_number = models.IntegerField(primary_key=True)
+    Seat_capacity = models.CharField(max_length=10, null=True)
+
+    def __str__(self):
+
+        return str(self.Room_number)
+
+
+class Meetingtime(models.Model):
+    Mid = models.IntegerField(primary_key=True)
+    time = models.TimeField(auto_now=False, auto_now_add=False, null=True)
+    day = models.DateField()
+
+    def __str__(self):
+
+        return str(self.time)
+
+
+class Department(models.Model):
+    dept_name = models.CharField(max_length=20, null=True)
+    Course = models.ForeignKey(Course, on_delete=models.CASCADE, null=True)
+
+    def __str__(self):
+
+        return self.dept_name
+
+
+class Section(models.Model):
+    Section_id = models.IntegerField(primary_key=True)
+    Department = models.ForeignKey(
+        Department, on_delete=models.CASCADE, null=True)
+    No_of_classin_week = models.CharField(max_length=20, null=True)
+    Meetingtime = models.ForeignKey(
+        Meetingtime, on_delete=models.CASCADE, null=True)
+    Room = models.ForeignKey(Room, on_delete=models.CASCADE, null=True)
+    instructors = models.ForeignKey(
+        Instructor, on_delete=models.CASCADE, null=True,blank=True)
+
+    def __str__(self):
+
+        return str(self.Room)
