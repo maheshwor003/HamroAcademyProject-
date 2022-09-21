6e67cff63672f2de6078d344739d4a752fd7aebc
kishor <kishorkadel42@gmail.com>
2022-09-21 16:16:05 +0545

    maheshor branch

--- SystemLogin/admin.py
+++ SystemLogin/admin.py
@@ -1,9 +1,12 @@
 from django.contrib import admin
-from .models import User
+from .models import Homedetails, User
 
 
 class AdminUser(admin.ModelAdmin):
-     list_display= ('username','is_staff','first_name','last_name','address','email','is_admin','is_student','is_teacher')
+    list_display = ('username', 'is_staff', 'first_name', 'last_name',
+                    'address', 'email', 'is_admin', 'is_student', 'is_teacher')
 
 
-admin.site.register(User,AdminUser)
+admin.site.register(User, AdminUser)
+
+admin.site.register(Homedetails)
--- SystemLogin/models.py
+++ SystemLogin/models.py
@@ -1,3 +1,6 @@
+from contextlib import nullcontext
+import email
+from statistics import mode
 from sys import maxsize
 from tokenize import blank_re
 from django.db import models
@@ -7,8 +10,18 @@ from django.contrib.auth.models import AbstractUser
 
 
 class User(AbstractUser):
-    is_admin= models.BooleanField('Is admin', default=False)
+    is_admin = models.BooleanField('Is admin', default=False)
     is_student = models.BooleanField('Is student', default=False)
     is_teacher = models.BooleanField('Is teacher', default=False)
-    address = models.CharField(max_length=50,null=True,blank=True)
+    address = models.CharField(max_length=50, null=True, blank=True)
 
+
+class Homedetails(models.Model):
+    comp_name = models.CharField(max_length=50, null=True, blank=True)
+    comp_title = models.CharField(max_length=1000, null=True, blank=True)
+    comp_title_two = models.CharField(max_length=1000, null=True, blank=True)
+    comp_details = models.TextField(max_length=2000, null=True, blank=True)
+    comp_details_two = models.TextField(max_length=2000, null=True, blank=True)
+    address = models.CharField(max_length=100, null=True, blank=True)
+    email = models.EmailField(max_length=100, null=True, blank=True)
+    notice = models.TextField(max_length=2000, null=True, blank=True)
